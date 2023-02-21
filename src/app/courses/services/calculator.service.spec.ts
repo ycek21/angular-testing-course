6985229e98cf066de50d8512b0e02025a3fe4a43
DMGMORI\eichlerj <jacek.eichler@dmgmori.com>
2023-02-21 18:27:56 +0100

    Lesson 13

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,26 +1,37 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
+import { TestBed } from "@angular/core/testing";
 
 describe("CalculatorService", () => {
+  let calculatorService: CalculatorService;
+  let logger: LoggerService;
+
+  beforeEach(() => {
+    logger = jasmine.createSpyObj("LoggerService", ["log"]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: logger },
+      ],
+    });
+
+    calculatorService = TestBed.inject(CalculatorService);
+  });
+
   it("should add two numbers", () => {
-    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
-
-    // spyOn(logger, "log");
-
-    const calculatorService = new CalculatorService(logger);
-
     const result = calculatorService.add(2, 2);
 
     expect(result).toBe(4);
 
-    expect(logger.log).toHaveBeenCalledTimes(2);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it("should add subtract numbers", () => {
-    const calculatorService = new CalculatorService(new LoggerService());
-
     const result = calculatorService.subtract(2, 2);
 
     expect(result).toBe(0, "unexpected subtraction result");
+
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 });
---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { CoursesService } from "./courses.service";
+import { COURSES } from "../../../../server/db-data";
+
+describe("CoursesService", () => {
+  let coursesService: CoursesService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        CoursesService,
+        // adding HttpClient would sent actual requests. We only want to mock it
+        // HttpClient
+      ],
+    });
+
+    coursesService = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it("should retrieve all courses", () => {
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy("No courses returned");
+      expect(courses.length).toBe(12, "Incorrect number of courses");
+
+      const course = courses.find((course) => course.id == 12);
+
+      expect(course.titles.description).toBe("Angular Testing Course");
+    });
+
+    const req = httpTestingController.expectOne("/api/courses");
+
+    expect(req.request.method).toEqual("GET");
+
+    req.flush({ payload: Object.values(COURSES) });
+  });
+});
